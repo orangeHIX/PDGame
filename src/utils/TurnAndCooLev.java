3fbeee55ab6ca7d83ecee0ea8939ee12e0af575d
orangeHIX <orangehix@gmail.com>
2015-05-07 20:25:31 +0800

    方便绘制折线图的类

---
+++ src/utils/TurnAndCooLev.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class TurnAndCooLev {
+
+	public int turn;
+	public float cooLev;
+	public TurnAndCooLev(int turn, float cooLev) {
+		super();
+		this.turn = turn;
+		this.cooLev = cooLev;
+	}
+	@Override
+	public String toString() {
+		return "TurnAndCooLev [turn=" + turn + ", cooLev=" + cooLev + "]";
+	}
+	
+}
